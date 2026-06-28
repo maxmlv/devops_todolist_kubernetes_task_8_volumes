75c4cfdcaa323d783f39fada752957391168e95c
maxmlv <max.mlv.k@gmail.com>
2026-06-28 20:03:42 +0300

    implement volumes to todoapp deployment

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+kubectl apply -f ./.infrastructure/namespace.yml
+
+kubectl apply -f ./.infrastructure/pv.yml
+kubectl apply -f ./.infrastructure/pvc.yml
+kubectl apply -f ./.infrastructure/configMap.yml
+kubectl apply -f ./.infrastructure/secret.yml
+
+kubectl apply -f ./.infrastructure/deployment.yml
+
+kubectl apply -f ./.infrastructure/clusterIp.yml
+kubectl apply -f ./.infrastructure/nodeport.yml
